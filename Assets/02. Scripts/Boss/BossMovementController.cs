c32a70d1c7a6d976764c38d465deae7e115f6c80
SeYunJung <77439732+SeYunJung@users.noreply.github.com>
2025-07-30 12:15:49 +0900

    [Update] 보스 공격 로직 수정
    - 콜리전 충돌 처리 코드를 삭제하고 공격범위를 설정해서 보스가 따라오도록 수정함.

--- Assets/02. Scripts/Boss/BossMovementController.cs
+++ Assets/02. Scripts/Boss/BossMovementController.cs
@@ -15,7 +15,7 @@ public class BossMovementController : MonoBehaviour
 {
     /*
      
-    구현 기능 : 보스가 플레이어 따라가기 
+    구현 기능1 : 보스가 플레이어 따라가기 
 
     필요한 것들 
     - 플레이어 따라가기 (O)
@@ -33,12 +33,35 @@ public class BossMovementController : MonoBehaviour
     - 보스와 플레이어의 거리를 계산한다. 
     - 보스가 플레이어를 바라보는 방향을 계산한다. 
     - 계산한 거리와 보스의 거리를 비교해서 거리 내로 들어오면 보스를 이동시킨다. 
+
+
+    구현 기능2 : 공격 딜레이 시간 
+    지금 구조는 ActionState.Attack 상태일 때 일반공격이나 특수공격을 계속 함. 
+    공격이 계속 들어가면 플레이어 hp가 연속으로 깎이니까 게임 밸런스가 안 맞고 이상함
+    공격에 딜레이 시간을 넣어서 공격이 계속 들어가지 못하게 만들자. 
+
+    필요한 것들 
+    - 공격 딜레이 시간 변수 
+        - 일반 공격 딜레이 시간(normalAttackDelayTime)
+        - 특수 공격 딜레이 시간(specialAttackDelayTime)
+    - 공격이 시작되면 공격 타입(일반 공격, 특수 공격)에 맞게 딜레이 시간을 부여해서
+      딜레이 시간 동안에는 공격할 수 없게 만들기 
+
+    구현 방법
+    - 공격 딜레이 시간 변수를 만든다. 
+    - 공격 시점에 딜레이 로직을 추가해준다. 
      
      */
+
+    // 플레이어 정보 
     public Transform player; // 플레이어 위치 
 
-    public float moveSpeed; // 보스 이동속도 
-    public float followDistance; // 보스 플레이어를 따라갈 수 있는 거리 (내가 정하는 것)
+    // 보스 정보 
+    public float moveSpeed; // 보스 이동속도 (내가 정하는 값)
+    public float health = 100;
+
+    public float followDistance; // 보스가 플레이어를 따라갈 수 있는 거리 (내가 정하는 값)
+    public float attackRange; // 보스가 플레이어를 공격할 수 있는 범위 (개가 정하는 값) 
     public Vector3 directionToTarget; // 보스가 플레이어를 바라보는 방향
     public float distanceToPlayer; // 보스와 플레이어 사이 거리 (계속 체크하는 값)
 
@@ -46,7 +69,8 @@ public class BossMovementController : MonoBehaviour
 
     public Rigidbody rigid;
 
-    public float health = 100;
+    public float normalAttackDelayTime; // 일반 공격 딜레이 시간 (내가 정하는 값)
+    public float specialAttackDelayTime; // 특수 공격 딜레이 시간 (내가 정하는 값)
 
     private void Awake()
     {
@@ -62,8 +86,6 @@ public class BossMovementController : MonoBehaviour
         distanceToPlayer = Vector3.Distance(transform.position, player.position); // 보스와 플레이어 사이 거리
         directionToTarget = (player.position - transform.position).normalized; // 보스가 플레이어를 바라보는 방향
 
-        
-
         // 플레이어가 보스가 따라갈 수 있는 거리(followDistance)로 들어오면 
         if (distanceToPlayer <= followDistance)
         {
@@ -72,9 +94,19 @@ public class BossMovementController : MonoBehaviour
                 // Move 상태로 전환 
                 ChangeState(ActionState.Move); // FixedUpdate에서 이동시키기 
                 // 보스가 플레이어 위치에서 살짝 떨어진 거리까지 이동한다. (서로 붙는 정도) 
+
+                // 플레이어를 따라가다가 플레이어가 공격범위 내로 들어오면 
+                if(distanceToPlayer <= attackRange)
+                {
+                    // 공격모드로 전환 
+                    ChangeState(ActionState.Attack);
+                }
             }
         }
-        
+        else
+        {
+            ChangeState(ActionState.Idle);
+        }
 
         if (currentState == ActionState.Attack)
         {
@@ -82,20 +114,24 @@ public class BossMovementController : MonoBehaviour
             {
                 Debug.Log("일반공격"); // 일반공격
                 //TakeDamage(); // 데미지 입히는 메서드 호출 
+                // 딜레이 시간 부여 
                 // 애니메이션 적용
             }
             else
             {
                 Debug.Log("특수공격"); // 강공격, 멀리 있으면 도끼 던지기? 
                 //TakeDamage(); // 데미지 입히는 메서드 호출 
+                // 딜레이 시간 부여 
                 // 애니메이션 적용
             }
 
             Debug.Log("플레이어 HP가 줄어듦");
             // 플레이어 피가 0이 될때까지 계속 떼리기 
 
-            if (distanceToPlayer <= followDistance)
+            // 공격하고 있는데 플레이어가 공격 범위에서 멀어지면
+            if (distanceToPlayer > attackRange) // 플레이어와의 거리가 공격 범위보다 크다는 건 공격범위에서 멀어졌다는 것. 
             {
+                // Idle상태로 전환 
                 ChangeState(ActionState.Idle);
             }
         }
@@ -118,14 +154,6 @@ public class BossMovementController : MonoBehaviour
         if (currentState == state) return;
 
         this.currentState = state;
-    }
-
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            ChangeState(ActionState.Attack);
-        }
     }
 
     //private void TakeDamage(Player player)
