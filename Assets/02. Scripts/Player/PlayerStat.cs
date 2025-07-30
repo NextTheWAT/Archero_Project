ce8eb3defa5c3e1ab99d3e7871c7bf4a3f6f4988
Lee JaeEun <62467424+NextTheWAT@users.noreply.github.com>
2025-07-30 12:56:40 +0900

    [Update] Player Hp Bar 추가 / 나머지 UI도 Player Stat 적용 완료

--- Assets/02. Scripts/Player/PlayerStat.cs
+++ Assets/02. Scripts/Player/PlayerStat.cs
@@ -10,12 +10,6 @@ public class PlayerStat : MonoBehaviour
     public int currentHp = 100;
     public int projectileCount = 1;
 
-    public void Heal(float amount)
-    {
-        currentHp += Mathf.RoundToInt(amount);
-        if (currentHp > maxHp) currentHp = maxHp;
-        Debug.Log($"체력 회복됨! 현재 체력: {currentHp}");
-    }
 
     private void Start()
     {
@@ -27,6 +21,12 @@ public class PlayerStat : MonoBehaviour
         {
             Debug.LogWarning("SkillUIManager.Instance is null. ShowSkillUI() 호출 실패");
         }
+    }
+    public void Heal(float amount)
+    {
+        currentHp += Mathf.RoundToInt(amount);
+        if (currentHp > maxHp) currentHp = maxHp;
+        Debug.Log($"체력 회복됨! 현재 체력: {currentHp}");
     }
 
 }
