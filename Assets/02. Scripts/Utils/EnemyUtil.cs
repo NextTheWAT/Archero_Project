61bd7662b7fdf685aa998b6a5f197050414de173
Lee JaeEun <62467424+NextTheWAT@users.noreply.github.com>
2025-08-01 10:43:28 +0900

    [Fix] Boss 태그 추가, EnemyUtil - Boss 태그 추가

--- Assets/02. Scripts/Utils/EnemyUtil.cs
+++ Assets/02. Scripts/Utils/EnemyUtil.cs
@@ -5,9 +5,12 @@ public static class EnemyUtil
     public static GameObject FindNearestEnemy(Vector3 from)
     {
         GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        GameObject[] bosses = GameObject.FindGameObjectsWithTag("Boss");
+
         GameObject nearest = null;
         float minDist = float.MaxValue;
 
+        // Enemy 검색
         foreach (GameObject enemy in enemies)
         {
             float dist = (enemy.transform.position - from).sqrMagnitude;
@@ -17,6 +20,18 @@ public static class EnemyUtil
                 nearest = enemy;
             }
         }
+
+        // Boss 검색
+        foreach (GameObject boss in bosses)
+        {
+            float dist = (boss.transform.position - from).sqrMagnitude;
+            if (dist < minDist)
+            {
+                minDist = dist;
+                nearest = boss;
+            }
+        }
+
         return nearest;
     }
 }
