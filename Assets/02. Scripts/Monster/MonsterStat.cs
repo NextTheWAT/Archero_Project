97a35473ea4d9436788993791c150c7138b4a5ab
Lee JaeEun <62467424+NextTheWAT@users.noreply.github.com>
2025-08-01 15:09:45 +0900

    [Update] Monste Sound 추가완료

--- Assets/02. Scripts/Monster/MonsterStat.cs
+++ Assets/02. Scripts/Monster/MonsterStat.cs
@@ -19,6 +19,7 @@ public class MonsterStat : MonoBehaviour
     }
     public void TakeDamage(float damage)
     {
+        SoundManager.Instance.Monster_SFX(1); // 몬스터 히트 사운드 재생
         currentHp -= damage;
         if (currentHp <= 0)
         {
