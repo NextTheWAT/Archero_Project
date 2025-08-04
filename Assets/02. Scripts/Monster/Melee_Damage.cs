49c33be6e2acc208b7da5b1ec1758c88cef3aeb0
nolabong1 <noano1@naver.com>
2025-08-04 14:45:15 +0900

    적이 죽으면 데미지를 입힐 수 없게 수정

--- Assets/02. Scripts/Monster/Melee_Damage.cs
+++ Assets/02. Scripts/Monster/Melee_Damage.cs
@@ -19,12 +19,15 @@ public class Melee_Damage : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        // 적이 죽었으면 데미지 처리하지 않음
+        if (monsterFSM.isDie) return;
+
         if (other.CompareTag("Player") && !isDamagedDelay && monsterFSM.isAnimDamage == false)
         {
             isDamagedDelay = true;
             playerStat.Damage(10f);
             Debug.Log("플레이어가 근접 공격을 받았습니다.");
-            StartCoroutine(DelayisDamage()); // 1.5초 후에 다시 데미지를 입힐 수 있도록 설정
+            StartCoroutine(DelayisDamage());
         }
     }
 
