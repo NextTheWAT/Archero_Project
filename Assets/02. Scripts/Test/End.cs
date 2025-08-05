fc0d5f1a0ca91e3110d34392a516176c47d90634
유경 <saa1007@naver.com>
2025-08-05 09:35:53 +0900

    엔딩씬 최종 수정

--- Assets/02. Scripts/Test/End.cs
+++ Assets/02. Scripts/Test/End.cs
@@ -5,11 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class End : MonoBehaviour
 {
-
-
     private void OnTriggerEnter(Collider other)
     {
-        SceneManager.LoadScene(0);
+        if (other.CompareTag("Player")) // 플레이어와 충돌했을 때만
+        {
+            SceneManager.LoadScene("EndingScenes");
+        }
     }
-
 }
